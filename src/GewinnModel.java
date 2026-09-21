806464178725c4a58d3313853943c8927c759909
Larisa Likic <larisa.likic@gmail.com>
2026-09-21 18:31:21 +0200

    Create Constructor, Getter and Computer Number Calculator

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -2,6 +2,30 @@ import java.util.Random;
 
 public class GewinnModel {
 
+    private int gesamtPunkte;
+    private int spielerZahl;
+    private int computerZahl;
+    private int rundenErgebnis;
 
+    public GewinnModel() {
+        gesamtPunkte = 30;
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
+
+    public void berechneComputerZahl(){
+        Random random = new Random();
+        computerZahl = random.nextInt(9) + 1;
+    }
 
 }
