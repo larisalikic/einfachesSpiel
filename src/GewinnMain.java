c73ac692cd51d877c2084b34ea288d91bc9c81bd
Larisa Likic <larisa.likic@gmail.com>
2026-09-21 20:33:18 +0200

    Main: Model, View and Controller connected and Game start

---
+++ src/GewinnMain.java
@@ -0,0 +1,8 @@
+public class GewinnMain {
+    public static void main(String[] args) {
+        GewinnModel model = new GewinnModel();
+        GewinnView view = new GewinnView();
+        new GewinnController(model, view);
+        view.setVisible(true);
+    }
+}
